8bdd840a797251b7c3cbc8ab48854574390d3de2
kthatoto <kthatoto@gmail.com>
2024-02-10 15:57:48 +0900

    Add link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,28 @@
-import { Button } from "@mantine/core";
+import { AppShell, Button, Card, Space, Text } from "@mantine/core";
 import SignIn from "@/components/SignIn";
 import useAuthStore from "@/stores/authStore";
+import { Link } from "react-router-dom";
 
 const Index = () => {
   const { user, signOut } = useAuthStore();
 
-  if (user) {
-    return (
-      <Button onClick={signOut}>Sing Out</Button>
-    );
-  }
-  return <SignIn />;
+  return (
+    <AppShell>
+      <AppShell.Main bg="#efefef" p={40}>
+        {user ? (
+          <Button onClick={signOut}>Sing Out</Button>
+        ) : (
+          <SignIn />
+        )}
+        <Space h={40} />
+        <Link to="/ships/1">
+          <Card shadow="sm" w={300} h={100}>
+            <Text>First Ship</Text>
+          </Card>
+        </Link>
+      </AppShell.Main>
+    </AppShell>
+  )
 };
 
 export default Index;
