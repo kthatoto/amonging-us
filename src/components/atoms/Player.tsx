05c87378725121fc8005b76911506e995cc28548
kthatoto <kthatoto@gmail.com>
2024-02-10 16:03:53 +0900

    Improve text icon

--- src/components/atoms/Player.tsx
+++ src/components/atoms/Player.tsx
@@ -2,9 +2,10 @@ import { Box, Image, Text } from "@mantine/core";
 import useAuthStore from "@/stores/authStore";
 import { useController } from "@/hooks/useController";
 
+const ICON_SIZE = 50;
 const commonStyle = {
-  width: 50,
-  height: 50,
+  width: ICON_SIZE,
+  height: ICON_SIZE,
   margin: "auto",
   inset: 0,
   position: "absolute",
@@ -36,7 +37,11 @@ const Player = () => {
       {user.photoURL ? (
         <Image w="100%" h="100%" radius="50%" src={user.photoURL} />
       ) : (
-        <Text>{user.displayName?.at(0)}</Text>
+        <Text
+          ta="center"
+          fw="bold"
+          style={{ lineHeight: ICON_SIZE + "px", fontSize: 24 }}
+        >{user.displayName?.at(0)}</Text>
       )}
     </Box>
   );
