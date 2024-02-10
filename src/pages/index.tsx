60beb9ab3727d34ba8578c3b809af76a6eb346f4
kthatoto <kthatoto@gmail.com>
2024-02-11 02:54:32 +0900

    Add new ship button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { AppShell, Button, Card, SimpleGrid, Space, Text } from "@mantine/core";
+import { AppShell, Button, Card, Center, SimpleGrid, Space, Text } from "@mantine/core";
+import { FontAwesomeIcon as I } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
 import SignIn from "@/components/SignIn";
 import useAuthStore from "@/stores/authStore";
 import useShipsStore from "@/stores/shipsStore";
@@ -19,9 +21,15 @@ const Index = () => {
         {user ? <Button onClick={signOut}>Sing Out</Button> : <SignIn />}
         <Space h={40} />
         <SimpleGrid cols={3}>
+          <Card shadow="sm" h={200} style={{ cursor: "pointer" }}>
+            <Center h="100%">
+              <I icon={faPlus} size="xl" />
+              <Text fz={24} ml={10}>New Ship</Text>
+            </Center>
+          </Card>
           {ships.map((ship) => (
             <Link key={ship.id} to={`/ships/${ship.id}`}>
-              <Card shadow="sm" h={100}>
+              <Card shadow="sm" h={200}>
                 <Text>{ship.title}</Text>
               </Card>
             </Link>
