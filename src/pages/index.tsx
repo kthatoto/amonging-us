deda8234b13c33428e7dcfd82f7eb81d701b2289
kthatoto <kthatoto@gmail.com>
2024-02-10 17:01:27 +0900

    Fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,7 @@ const Index = () => {
         <Space h={40} />
         <SimpleGrid cols={3}>
           {ships.map((ship) => (
-            <Link to={`/ships/${ship.id}}}`}>
+            <Link to={`/ships/${ship.id}`}>
               <Card shadow="sm" h={100}>
                 <Text>{ship.title}</Text>
               </Card>
