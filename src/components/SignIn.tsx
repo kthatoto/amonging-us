bfa904b1c54e310e8432163f18662b3efef7a650
kthatoto <kthatoto@gmail.com>
2024-02-11 06:57:09 +0900

    Fix

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,14 +1,12 @@
-import { Button, Stack } from "@mantine/core";
+import { Button } from "@mantine/core";
 import useAuthStore from "@/stores/authStore";
 
 const SignIn = () => {
   const { signInWithGoogle } = useAuthStore();
   return (
-    <Stack>
-      <Button onClick={signInWithGoogle} fullWidth>
-        Googleログイン
-      </Button>
-    </Stack>
+    <Button onClick={signInWithGoogle}>
+      Googleログイン
+    </Button>
   );
 };
 export default SignIn;
