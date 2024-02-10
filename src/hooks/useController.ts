a4994e4816842db27da3d880ee8294422abb0204
kthatoto <kthatoto@gmail.com>
2024-02-10 14:46:59 +0900

    move player

--- src/hooks/useController.ts
+++ src/hooks/useController.ts
@@ -5,6 +5,11 @@ import { useInterval } from "@/utils/useInterval";
 const INTERVAL = 100;
 const TICK_DISTANCE = 3;
 
+const UP_KEYS = ["ArrowUp", "w"];
+const DOWN_KEYS = ["ArrowDown", "s"];
+const RIGHT_KEYS = ["ArrowRight", "d"];
+const LEFT_KEYS = ["ArrowLeft", "a"];
+
 export const useController = () => {
   const { move } = usePlayerStore();
 
@@ -21,6 +26,17 @@ export const useController = () => {
 
   useInterval(
     async () => {
+      const isUp = UP_KEYS.some((k) => pressingKeys.includes(k));
+      const isDown = DOWN_KEYS.some((k) => pressingKeys.includes(k));
+      const isRigth = RIGHT_KEYS.some((k) => pressingKeys.includes(k));
+      const isLeft = LEFT_KEYS.some((k) => pressingKeys.includes(k));
+
+      const diff = { x: 0, y: 0 };
+      if (isUp) diff.y -= TICK_DISTANCE;
+      if (isDown) diff.y += TICK_DISTANCE;
+      if (isRigth) diff.x += TICK_DISTANCE;
+      if (isLeft) diff.y -= TICK_DISTANCE;
+      move(diff.x, diff.y);
     },
     INTERVAL
   );
