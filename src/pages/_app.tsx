9f818e3bdca6e15a8f90c13b48a69cd8a7c52f20
kthatoto <kthatoto@gmail.com>
2024-02-11 12:56:16 +0900

    Fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import { Outlet } from "react-router-dom";
 import { createTheme, MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
-// import useAuthStore from "@/stores/authStore";
 
 const globalFontFamily =
   'Avenir, "Helvetica Neue", Helvetica, Arial, "Hiragino Kaku Gothic ProN", YuGothic';
